1d358e067573e95540b48fdba1c9ca01b64a6242
Maxi160108 <maxdiaz@liceovvh.cl>
2025-03-10 11:14:12 -0300

    ok

---
+++ JS/Ejercicio/01_variables/script.js
@@ -0,0 +1,27 @@
+function numero(){
+    alert(1)
+};
+
+function decimal(){
+    alert(1.5)
+};
+
+function texto(){
+    alert("Hola, soy un texto")
+};
+
+function booleano(){
+    alert(true)
+};
+
+function array(){
+    alert()
+};
+
+function objeto(){
+    alert()
+};
+
+function decimal(){
+    alert()
+};
