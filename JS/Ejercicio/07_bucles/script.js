3cdf79952a2be642fdc4760b25b69b5972f53791
Maxi160108 <166129103+Maxi160108@users.noreply.github.com>
2025-04-09 16:09:16 -0400

    Add files via upload

---
+++ JS/Ejercicio/07_bucles/script.js
@@ -0,0 +1,27 @@
+//while
+
+function bucleWhile(){
+
+let contador = 10;
+let array = [];
+
+while (contador >= 1) {
+    
+    array.push(contador);
+
+    contador--;
+}
+    alert(`Los numeros son: ${array.join(" - ")}`);
+}
+
+//for
+
+function bucleFor(){
+for (let i = 10; i >= 1; i--) {
+
+    array.push(i);
+    
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`);
+}
+
---
+++ JS/Ejercicio/trabajo_grupo_01/script.js
@@ -0,0 +1,34 @@
+function generadorNumerosPrimos() {
+    // Solicitar número límite al usuario
+    let limite = parseInt(prompt("Ingrese un numero limite para la impresión: "))
+    let arrayPrimos = []
+
+    // Validación de entrada
+    if (limite === 0 || limite === 1 || limite < 0) {
+        alert("Ingrese un valor valido")
+    }
+
+    // Búsqueda de números primos hasta el límite
+    for(let i = 2; i < limite + 1; i++) {
+        if(esPrimo(i)) {
+            arrayPrimos.push(i)
+        }
+    }
+
+    // Función auxiliar para verificar si un número es primo
+    function esPrimo(numero) {
+        for(let j = 2; j < numero; j++) {
+            if(numero % j === 0) {
+                return false;
+            }
+        }
+        return numero !== 1;
+    }
+
+    // Validación final y muestra de resultados
+    if(limite === 0 || limite === 1 || limite < 0) {
+        alert("No se ha generado nada")
+    } else {
+        alert(`Los numeros primos al numero que ingresaste son: ${arrayPrimos.join(" - ")}`)
+    }
+}
