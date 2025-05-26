e196019b54222e98d0a59457fa1fc148c716e254
Maxi160108 <166129103+Maxi160108@users.noreply.github.com>
2025-05-26 14:47:01 -0400

    Update script.js

--- JS/Ejercicio/7.6_Practicando/script.js
+++ JS/Ejercicio/7.6_Practicando/script.js
@@ -1,19 +1,36 @@
-//Primero
-
-function tablaMultiplicar() {
-    let cifra = prompt('Ingrese su numero: ');
-    
-    let multiplicador = 1; 
-
-    while(multiplicador <= 10) {
-        let resultado = cifra * multiplicador;
-        alert(cifra + 'x' + multiplicador + '=' + resultado);
-        multiplicador++;
+function multiplicar() {
+    let num = parseInt(prompt("Ingresa tu numero: "));
+    let datos = [];
+    let resultados = 0;
+    for (let mult = 1; mult <= 10; mult++) {
+        resultados = num * mult;
+        datos.push(`${mult} * ${num} = ${resultados}`);
+        if(mult >= 10){
+        alert(datos.join(`\n`));
+        }
     }
 }
 
-//Segundo
+function ingresar() {
+    let n = parseInt(prompt("Ingresa tu numero: "));
+    let last = 0;
+    for(let num = 1; num <= n; num++){
+        last += num
+        if(num >= n) {
+            alert(`El resultado es: ${last}`);
+        }
+    }
+}
 
-function suma(){
-    
+function pares() {
+    let num = parseInt(prompt("Ingrese su primer numero: "));
+    let num2 = parseInt(prompt("Ingrese su ultimo numero: "));
+    let array = []
+    for(let n = 0;num <= num2; num++){
+        if (num % 2 == 0) {
+            n = num
+            array.push(n);
+            alert(array);
+        }
+    }
 }
